38190bcdcd2244a00ac7a37cb90b05979e9956d4
tsekamaru <tseka.nice@gmail.com>
2024-12-21 14:44:37 +0100

    Changed folder names to lower case

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -1,9 +1,9 @@
 import { useState } from "react";
 import "./App.css";
 
-import Sidebar from "./Components/Sidebar/Sidebar";
-import Footer from "./Components/Footer/Footer";
-import Navbar from './Components/Navbar/Navbar'
+import Sidebar from "./Components/sidebar/Sidebar";
+import Footer from "./Components/footer/Footer";
+import Navbar from './Components/navbar/Navbar'
 
 
 function App() {
