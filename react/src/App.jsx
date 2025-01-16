b261fae992dd78280a97719ed5c13ec07f0f9cf7
tsekamaru <tseka.nice@gmail.com>
2025-01-16 21:38:05 +0100

    Small fix on App.jsx

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -7,29 +7,23 @@ import { Routes, Route } from "react-router";
 import AboutPage from "./pages/Aboutpage";
 import ErrorPage from "./pages/Errorpage";
 import Apartments from "./pages/Apartments";
-import listings from "./data/listings.json"; 
+import listings from "./data/listings.json";
 import { useState } from "react";
-
 
 function App() {
   const [itemList] = useState(listings.results);
   return (
     <div className="App">
       <Navbar />
-      
+
       <Routes>
-        <Route path="/" element={<Homepage />} />
+        <Route path="/" element={<Homepage itemList={itemList} />} />
         <Route path="/about" element={<AboutPage />} />
         <Route path="/apartments" element={<Apartments />} />
-        <Route path="*" element={<ErrorPage />}/>
+        <Route path="*" element={<ErrorPage />} />
       </Routes>
       <div id="main-content">
-        <div id="pages">
-
-          <Homepage itemList={itemList} />
-
-        </div>
-        
+        <div id="pages"></div>
       </div>
       <Footer />
     </div>
